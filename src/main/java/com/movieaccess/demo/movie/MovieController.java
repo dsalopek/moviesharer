fad4ac72754223eea0c6601c2978fbc68dda96d1
Dylan <dylan.salopek@gmail.com>
2019-09-24 19:00:06 -0500

    Initial commit

---
+++ src/main/java/com/movieaccess/demo/movie/MovieController.java
@@ -0,0 +1,44 @@
+package com.movieaccess.demo.movie;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class MovieController {
+
+    private MovieService movieService;
+
+    @Autowired
+    public MovieController(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+    @GetMapping("/movies")
+    public List<Movie> getAllMovies(){
+        return this.movieService.getAllMovies();
+    }
+
+    @GetMapping("/movies/{id}" )
+    public Movie getSingleMovie(@PathVariable(value = "id") int id){
+        return this.movieService.getOneMovie(id);
+    }
+
+    @PostMapping("/movies")
+    public void addMovie(@RequestBody Movie movie){
+        this.movieService.addMovie(movie);
+    }
+
+    @PutMapping("/movies/{id}")
+    public void updateMovie(@PathVariable(value = "id") int id, @RequestBody Movie movie){
+        Movie tempMovie = new Movie();
+        tempMovie.setId(id);
+        tempMovie.setImdbId(movie.getImdbId());
+        tempMovie.setAddedDate(movie.getAddedDate());
+        tempMovie.setAddedBy(movie.getAddedBy());
+        tempMovie.setActive(movie.isActive());
+        this.movieService.updateMovie(tempMovie);
+    }
+
+}
