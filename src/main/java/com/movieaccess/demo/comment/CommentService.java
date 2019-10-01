83310367a4e741829cc82d91a3ef90990cd9bb4e
Dylan <dylan.salopek@gmail.com>
2019-10-01 18:19:36 -0500

    Added Basic error handling to movie, comment
    Added @ControllerAdvice for exception handling within Movie and Comment classes

--- src/main/java/com/movieaccess/demo/comment/CommentService.java
+++ src/main/java/com/movieaccess/demo/comment/CommentService.java
@@ -1,8 +1,11 @@
 package com.movieaccess.demo.comment;
 
+import com.movieaccess.demo.exception.CommentNotFoundException;
+import com.movieaccess.demo.movie.Movie;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -16,21 +19,35 @@ public class CommentService {
         this.commentRepository = commentRepository;
     }
 
-    public List<Comment> getAllComments(int movieId){
+    public List<Comment> getAllComments(int movieId) {
         List<Comment> comments = new LinkedList<Comment>();
         commentRepository.findAllByMovieId(movieId).forEach(comments::add);
         return comments;
     }
 
-    public Comment getOneComment(int commentId){
-        return commentRepository.findById(commentId).get();
+    public Comment getOneComment(int commentId) {
+        return commentRepository.findById(commentId)
+                .orElseThrow(() -> new CommentNotFoundException("Comment not found for commentId: " + commentId));
     }
 
-    public void addComment(Comment comment){
-        commentRepository.save(comment);
+    public Comment addComment(Comment comment) {
+        return commentRepository.save(comment);
     }
 
-    public void updateComment(Comment comment){
-        commentRepository.save(comment);
+    public Comment updateComment(Comment newComment, int commentId, int movieId) {
+        return commentRepository.findById(newComment.getId())
+                .map(comment -> {
+                    comment.setContent(newComment.getContent());
+                    comment.setMovie(new Movie(movieId));
+                    comment.setAddedDate(new Date(System.currentTimeMillis()));
+                    comment.setAddedBy("dylan");
+                    comment.setActive(newComment.isActive());
+                    return commentRepository.save(comment);
+                })
+                .orElseGet(() -> {
+                    newComment.setId(commentId);
+                    newComment.setMovie(new Movie(movieId));
+                    return commentRepository.save(newComment);
+                });
     }
 }
