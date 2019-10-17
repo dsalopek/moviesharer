702c91864b4505e9bdc3198e2fb54c6b8b35313e
Dylan <dylan.salopek@gmail.com>
2019-10-17 06:59:34 -0500

    Updated UserDetailServiceImpl

--- src/main/java/com/movieaccess/rest/security/JwtAuthenticationFilter.java
+++ src/main/java/com/movieaccess/rest/security/JwtAuthenticationFilter.java
@@ -1,6 +1,6 @@
 package com.movieaccess.rest.security;
 
-import com.movieaccess.rest.service.UserServiceImpl;
+import com.movieaccess.rest.service.UserDetailServiceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,7 +22,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private JwtTokenProvider tokenProvider;
 
     @Autowired
-    private UserServiceImpl userServiceImpl;
+    private UserDetailServiceImpl userServiceImpl;
 
     private static final Logger logger = LoggerFactory.getLogger(JwtAuthenticationFilter.class);
 
