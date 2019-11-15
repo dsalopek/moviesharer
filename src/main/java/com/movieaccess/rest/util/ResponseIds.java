dea84c558f74d10ed323c15f9112098ad274c574
Andrew Salopek <dylan.salopek@gmail.com>
2019-11-15 07:15:32 -0600

    Model updates

---
+++ src/main/java/com/movieaccess/rest/util/ResponseIds.java
@@ -0,0 +1,8 @@
+package com.movieaccess.rest.util;
+
+public final class ResponseIds {
+    public static final int DECLINE = 1;
+    public static final int ACCEPT = 2;
+    public static final int TENTATIVE = 3;
+    public static final int NORESPONSE = 4;
+}
